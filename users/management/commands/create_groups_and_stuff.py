33300652714bf4f2c8eca560654ca963b66bb5b1
IgorK78 <i.kolegov78@gmail.com>
2023-12-13 15:12:38 +0300

    edit

--- users/management/commands/create_groups_and_stuff.py
+++ users/management/commands/create_groups_and_stuff.py
@@ -6,26 +6,11 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        moderator_group = Group.objects.create(name='moderator')
-        moderator_group.permissions.add(
-            Permission.objects.filter(codename='moderator').first()
-        )
-        moderator_group.save()
-
         content_manager_group = Group.objects.create(name='content_manager')
         content_manager_group.permissions.add(
             Permission.objects.filter(codename='content_manager').first()
         )
         content_manager_group.save()
-
-        moderator = User.objects.create(
-            email='moderator@email.com',
-            first_name='moderator',
-            last_name='moderator',
-        )
-        moderator.set_password('12345')
-        moderator.groups.add(moderator_group)
-        moderator.save()
 
         content_manager = User.objects.create(
             email='content_manager@email.com',
